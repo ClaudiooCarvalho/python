81e20c6a14cc57f059613b078ec5f5b0683beeb4
ClaudiooCarvalho <claudioscarvalho@gmail.com>
2023-11-09 17:25:33 -0300

    first commit

---
+++ Exercícios/ex002.py
@@ -0,0 +1,5 @@
+num1 = int(input('digite um número: '))
+num2 = int(input('digite outro número: ')) 
+s = num1 + num2
+
+print ('A soma entre {} e {} í igual a {}!'.format (num1, num2, s)) 
---
+++ Treinamento.py
@@ -0,0 +1,4 @@
+num1 = input('digite um número = ')
+num2 = input('digite outro número = ')
+
+print ('A soma é',num1+num2)
